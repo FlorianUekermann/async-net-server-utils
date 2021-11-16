90526c16bbd4226b37ec6d30e61b33e86fdda14e
Florian Uekermann <florian@uekermann.me>
2021-11-16 23:35:18 +0100

    remove debug prints

--- src/http.rs
+++ src/http.rs
@@ -27,22 +27,17 @@ impl<IO: AsyncRead + AsyncWrite + Unpin, T: Stream<Item = IO> + Unpin> Stream
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         loop {
-            dbg!();
             match self.decoding.poll_next_unpin(cx) {
                 Poll::Ready(Some(Ok(request))) => {
-                    dbg!("decoded");
                     return Poll::Ready(Some(request));
                 }
                 Poll::Ready(Some(Err(err))) => log::error!("http decoding error: {:?}", err),
                 Poll::Ready(None) | Poll::Pending => match self.incoming.poll_next_unpin(cx) {
-                    Poll::Ready(Some(transport)) => {
-                        dbg!("add");
-                        self.decoding
-                            .push(RequestHeadDecoder::default().decode(transport))
-                    }
+                    Poll::Ready(Some(transport)) => self
+                        .decoding
+                        .push(RequestHeadDecoder::default().decode(transport)),
                     Poll::Ready(None) => return Poll::Ready(None),
                     Poll::Pending => {
-                        dbg!("pending");
                         return Poll::Pending;
                     }
                 },
